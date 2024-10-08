fa60e12751990278691853b6e7783698143b63bb
Night1402 <romazvvp@gmail.com>
2024-10-08 13:31:47 +0500

    s

--- Project1/cod.cpp
+++ Project1/cod.cpp
@@ -1,70 +1,43 @@
 #include <iostream>
 #include <Windows.h>
 
+
+float Plus(float num1, float num2)
+{
+	std::cout << num1 + num2 << "\n";
+	return num1 + num2;
+}
+float Minus(float num1, float num2)
+{
+	std::cout << num1 - num2 << "\n";
+	return num1 - num2;
+}
+float Multiply(float num1, float num2)
+{
+	std::cout << num1 * num2 << "\n";
+	return num1 * num2;
+}
+float Divide(float num1, float num2)
+{
+	std::cout << num1 / num2 << "\n";
+	return num1 / num2;
+}
+float Percentage(float num1, float num2)
+{
+	std::cout << (num1 * num2) / 100 << "\n";
+	return (num1 * num2) / 100;
+}
+
 int main()
 {
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	double rub = 0;
-	double rub_s_com = 0;
-	double com = 0;
-	double itog = 0;
-	int val = 0;
+	Plus(2, 2);
+	Minus(5, 4);
+	Multiply(2, 4);
+	Divide(6, 3);
+	Percentage(5, 60);
 
-	while (true)
-	{
-		std::cout << "Введите сумму денег или 0 для завершения: ";
-		std::cin >> rub;
-
-		if (rub == 0)
-		{
-			break;
-		}
-
-		std::cout << "Введите валюту\n1.Доллор\n2.Евро\n3.Юань\n4.Фарит\n5.Йена\n";
-		std::cin >> val;
-
-		com = rub * 0.05;
-		rub_s_com = rub - com;
-		
-		if (rub <= 0)
-		{
-			std::cout << "У вас недостаточно стредств";
-			break;
-		}
-
-		else if (val == 1)
-		{
-			itog = rub_s_com / 92.92;
-			std::cout << "Вы получили " << itog << " Доллоров";
-		}
-
-		else if (val == 2)
-		{
-			itog = rub_s_com / 103.22;
-			std::cout << "Вы получили " << itog << " Евро";
-		}
-
-		else if (val == 3)
-		{
-			itog = rub_s_com / 13.11;
-			std::cout << "Вы получили " << itog << " Юаней";
-		}
-
-		else if (val == 4)
-		{
-			std::cout << "Вы слишком бедный для использования этой валюты";
-		}
-
-		else if (val == 5)
-		{
-			itog = rub_s_com / 0.65143;
-			std::cout << "Вы получили " << itog << " Йен";
-		}
-		std::cout << "\nКомиссия " << com << " Рублей\n";
-	}
-	
-	
-		return 0;
+	return 0;
 }
